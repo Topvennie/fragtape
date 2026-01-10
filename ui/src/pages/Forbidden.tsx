6dfb5dc2f3de2ce0aca263f3d6662f27fff320f8
vincent <vincent@vallaeys.com>
2026-01-10 21:48:33 +0100

    feat(server): forbidden page

--- ui/src/pages/Forbidden.tsx
+++ ui/src/pages/Forbidden.tsx
@@ -1,23 +1,28 @@
-import { Button, Center, Stack, Text, Title } from "@mantine/core";
+import { Title } from "@/components/atoms/Title";
+import { useAuth } from "@/lib/hooks/useAuth";
+import { Button, Center, Stack } from "@mantine/core";
 import { useNavigate } from "@tanstack/react-router";
+import { LuArrowLeft, LuLock } from "react-icons/lu";
 
 export const Forbidden = () => {
+  const { logout } = useAuth()
   const navigate = useNavigate()
 
   const handleReturn = () => {
-    navigate({ to: "." })
+    logout()
+    navigate({ to: "/" })
   }
 
   return (
-    <Center h="100%">
-      <Stack align="center" gap={0}>
-        <Text fw={600}>403</Text>
-        <Title fw={600} className="mt-2">Forbidden</Title>
-        <Text c="gray" className="mt-6">Not enough permissions</Text>
-        <Button onClick={handleReturn} className="mt-6">
-          Return to the start
-        </Button>
+    <Center h="100vh">
+      <Stack align="center" gap="xl">
+        <div className="p-4 rounded-full bg-(--mantine-color-primary-2) border-2 border-(--mantine-color-primary-4)">
+          <LuLock className="size-16 text-(--mantine-color-primary-6)" />
+        </div>
+        <Title order={2} className="text-center">Restricted Area</Title>
+        <p className="text-secondary whitespace-pre-wrap text-center">{`You don't have access to this area.`}</p>
+        <Button onClick={handleReturn} leftSection={<LuArrowLeft />}>Go back home</Button>
       </Stack>
     </Center>
-  );
+  )
 }
